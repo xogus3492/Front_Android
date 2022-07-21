4285b9ae4346210512a9d188981f4be22939ceb8
1000kkannoo <[hwsa10041@gmail.com]>
2022-07-21 14:27:59 +0900

    welcome android

--- app/src/main/java/com/example/fybproject/MainActivity.java
+++ app/src/main/java/com/example/fybproject/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.fybproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView1 = findViewById(R.id.textview1);
+        textView1.setText("welcome android");
     }
 }
