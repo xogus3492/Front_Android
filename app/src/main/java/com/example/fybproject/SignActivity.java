7742ef4c6fd4a734a44efbe993ea7022d62bed6c
xogus3492 <wkdxogus3492@gmail.com>
2022-09-03 10:50:03 +0900

    스플래시 디자인 구현

--- app/src/main/java/com/example/fybproject/SignActivity.java
+++ app/src/main/java/com/example/fybproject/SignActivity.java
@@ -3,13 +3,12 @@ package com.example.fybproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class SignActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign);
     }
 }
