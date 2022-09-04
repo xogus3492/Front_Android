0f8122d463782945d4ae1ebe705577c46b27cddc
xogus3492 <wkdxogus3492@gmail.com>
2022-09-04 16:44:54 +0900

    로그인 화면 구현

---
+++ app/src/main/java/com/example/fybproject/KakaoSignInActivity.java
@@ -0,0 +1,14 @@
+package com.example.fybproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KakaoSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kakao_sign_in);
+    }
+}
---
+++ app/src/main/java/com/example/fybproject/LocalSignInActivity.java
@@ -0,0 +1,14 @@
+package com.example.fybproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocalSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_sign_in);
+    }
+}
