24173966f618c6dd98c0993eb950c84476e9166c
xogus3492 <wkdxogus3492@gmail.com>
2022-09-10 00:27:03 +0900

    메인 홈 프래그먼트

--- app/src/main/java/com/example/fybproject/MainActivity.java
+++ app/src/main/java/com/example/fybproject/MainActivity.java
@@ -1,15 +1,34 @@
 package com.example.fybproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.fybproject.main.fragment.MainHomeFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    MainHomeFragment mainHomeFragment;
+
+    FragmentManager fragmentManager;
+    FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+
+        mainHomeFragment = new MainHomeFragment();
+
+        transaction = fragmentManager.beginTransaction();
+
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameMainHome, mainHomeFragment).commitAllowingStateLoss();
     }
 }
